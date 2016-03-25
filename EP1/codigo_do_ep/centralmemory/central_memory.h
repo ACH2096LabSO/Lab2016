08ce56f6a352aac4ffead060e580581e5fa4f5f2
gukiboy <gustavokishima@gmail.com>
2016-03-25 00:02:02 +0000

    Adiciona arquivo de cabecalho para CM
    Funcoes a serem implementadas em futuro arquivo central_memory.c

---
+++ EP1/codigo_do_ep/centralmemory/central_memory.h
@@ -0,0 +1,68 @@
+/**********************************************************
+
+ Header da memoria central CM
+
+ Esse arquivo contem as funcoes e estruturas referentes
+ a memoria central do simulador BASYS desenvolvido.
+
+ Quem mexeu:
+
+  - Gustavo Kishima
+
+
+
+
+                             Data de criacao: 24 Mar 2016
+
+**********************************************************/
+
+
+/**********************************************************
+
+ Tipo para definir se a memoria fisica ou logica sera
+ inicializada
+
+**********************************************************/
+enum TYPE
+{
+    PHYSICAL,
+    LOGICAL
+}
+
+
+/**********************************************************
+
+ int init_memory(int size, int num_page, int page_size,
+                 TYPE type)
+
+ Inicializa a memoria simulada. A depender da flag 'type',
+ a memoria sera fisica ou logica
+
+ @param
+ int size      - Tamanho da memoria
+ int num_page  - Numero de paginas
+ int page_size - Tamanho das paginas
+ TYPE type     - Tipo da memoria
+
+ @return
+ 0  : Memoria inicializada com sucesso
+ -1 : Tamanho de pagina maior que tamanho da memoria ERRO
+ 1  : Tamanho de paginas nao alcancado - pouca memoria
+
+**********************************************************/
+
+int init_physical_mem(int size, int num_page, int page_size, TYPE type);
+
+/**********************************************************
+
+ int allocate_mem(int size, int job_id);
+
+ Aloca a memoria para algum processo.
+
+ @param
+ int size   - Tamanho da memoria
+ int job_id - Identificacao do processo
+
+***********************************************************/
+
+int allocate_mem(int size, int job_id);
