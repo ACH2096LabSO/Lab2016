07e94d61a8d017676aa6061d4441ecdab7bc6aba
leo <leo@debian-leo>
2016-03-28 21:37:28 -0300

    Um esqueleto para o loop principal do simulador

--- EP1/src/simulador.c
+++ EP1/src/simulador.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include "entrada.h"
 
+void simulate()
+{
+    int finishedJobs = 0;
+    process* assignedJob = NULL;    //job atualmente assinalado à CPU
+    int currentTime = waiting.first->arrive_time;   //tempo atual de clock
+
+    while(n > finishedJobs)    //enquanto ainda houver processo em uma das filas
+    {
+        if(assignedJob == NULL)
+        {
+            //evento 1
+        }
+
+        //possiveis outros eventos vao aqui
+
+        currentTime++;
+    }
+}
+
 int main(int argc, char *argv[]) // main para inicializar o programa. qualquer dúvida comentar no código
 {
-
     FILE *f = NULL;
     int i;
 
@@ -20,7 +38,8 @@ int main(int argc, char *argv[]) // main para inicializar o programa. qualquer d
 
     setWaitingList(f);
 
-// Seria interessante colocar o código para a execução da simulação aqui
+    simulate();
+
     fclose(f);
     return 0;
 }
