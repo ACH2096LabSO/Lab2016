271e62ed7c9f3430d6d0861810bd7c6ae46f4ed3
Brunno Bettini <brunnobb@gmail.com>
2016-03-29 08:18:51 -0300

    adcionada opção de impressao de dados da memoria

--- EP1/src/messages/messages.c
+++ EP1/src/messages/messages.c
@@ -5,6 +5,7 @@
 #include "messages.h"
 #include "../entrada/entrada.h"
 
+
 void printConfig(){
 
 #ifdef _MESSAGES_
@@ -24,8 +25,8 @@ void printConfig(){
 void printProcess(process* proc){
 
 #ifdef _MESSAGES_
-        printf("%s %i %s %i %s %i %s %i %s %i %s %i %s %i\n", "Processo:", proc->ID, "Arrive Time:", proc->arrive_time, "CPU Burst:", proc->CPU_burst,
-               "CPU Used:", proc->CPU_used, "Memory:", proc->memory, "Total IO:", proc->I_O, "IO Used:", proc->I_O_used);
+    printf("%s %i %s %i %s %i %s %i %s %i %s %i %s %i\n", "Processo:", proc->ID, "Arrive Time:", proc->arrive_time, "CPU Burst:", proc->CPU_burst,
+           "CPU Used:", proc->CPU_used, "Memory:", proc->memory, "Total IO:", proc->I_O, "IO Used:", proc->I_O_used);
 #endif
 
 }
@@ -34,7 +35,8 @@ void printProcessList(char* lineName, processLine pline){
 
 
 #ifdef _MESSAGES_
-    printf("%s\n", "Lista de Processos em ordem");
+    printf("%s %s %s\n", "Lista de Processos da lista", lineName, "em ordem");
+    if (pline.first==NULL) return;
     process *p = pline.first;
     while (p!=NULL){
         printProcess(p);
@@ -43,6 +45,53 @@ void printProcessList(char* lineName, processLine pline){
 #endif
 
 
+}
 
+
+void printMemoryFrameQueue(FREE_FRAMES *freeFrames){
+
+#ifdef _MESSAGES_
+
+    if (freeFrames->head==NULL) return;
+    FRAME *f = freeFrames->head;
+    printf("%s %i\n", "Imprimindo Lista de Frames - tamanho vazio - ", freeFrames->length);
+    while(f!=NULL){
+        printf("%s ", "Lista de frames");
+        printFrame(f);
+        f= f->next;
+    }
+#endif
+}
+
+void printFrame(FRAME *fr){
+#ifdef _MESSAGES_
+    if (fr!=NULL)
+        printf("%s %i %s %i %s %i\n", "Frame:", fr->id, "Adress:", fr->address, "Job:", fr->job_id);
+    else
+        printf("%s \n", "Frame NULL");
+#endif
+}
+
+
+void printMemoryMap(FRAME **memorymap){
+#ifdef _MESSAGES_
+    int i;
+
+    printf("%s \n", "Imprimindo Mapa da memoria");
+    for(i=0;i<mainMemorySize;i++){
+        printf("%s %i ", "Local no mapa ", i);
+        printFrame(memorymap[i]);
+    }
+
+#endif
+}
+
+void printOutOfMemory(int available, int required, int jobId){
+#ifdef _MESSAGES_
+
+    printf("%s %i %s %i %s %i\n", "Não há memória - Disponível:", available, "Requerida:", required, "Processo:", jobId);
+
+
+#endif
 }
 
--- EP1/src/messages/messages.h
+++ EP1/src/messages/messages.h
@@ -5,14 +5,19 @@
 
 #include "../simulador.h"
 #include "../cpu/include/process_struct.h"
+#include "../memory/include/central_memory.h"
 
 
 
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
-    void printConfig();
-    void printProcess(process* proc);
-    void printProcessList(char* lineName, processLine pline);
+void printConfig();
+void printProcess(process* proc);
+void printProcessList(char* lineName, processLine pline);
+void printMemoryFrameQueue(FREE_FRAMES *freeFrames);
+void printFrame(FRAME *fr);
+void printMemoryMap(FRAME **memorymap);
+void printOutOfMemory(int available, int required, int jobId);
 
 #endif //MESSAGES_H
